1e621793a0c297bb022795706507631966ed0e34
Arpad Varga <arpad.varga90@gmail.com>
2024-06-25 11:42:53 +0200

    rename mistyped

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { initFakeApi } from "./mirage.js";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import FurituresPages from "./Pages/Furnitures";
+import FurnituresPages from "./Pages/FurnituresPages";
 import RoomsPages from "./Pages/RoomsPages";
 
 import "./index.css";
@@ -18,7 +18,7 @@ const routes = createBrowserRouter([
   },
   {
     path: "/rooms/:id",
-    element: <FurituresPages />,
+    element: <FurnituresPages />,
   },
 ]);
 
