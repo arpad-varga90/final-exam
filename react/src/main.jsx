2fffed68c9fa43199b385bdaf494eb74de3015d0
Arpad Varga <arpad.varga90@gmail.com>
2024-06-25 11:05:05 +0200

    add reactRouter and TanstackQuery snippets

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,14 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import { initFakeApi } from "./mirage.js";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import FurituresPages from "./Pages/Furnitures";
+import RoomsPages from "./Pages/RoomsPages";
+
 import "./index.css";
 
 // don't touch this line
 initFakeApi();
 
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <RoomsPages />,
+  },
+  {
+    path: "/rooms/:id",
+    element: <FurituresPages />,
+  },
+]);
+
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={routes} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
