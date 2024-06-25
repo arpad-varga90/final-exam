a6433868eb415c83212e382095146f1b3b1c9e2f
Arpad Varga <arpad.varga90@gmail.com>
2024-06-25 09:33:49 +0200

    refactor for cleaner code

--- sitebuild/src/App.jsx
+++ sitebuild/src/App.jsx
@@ -1,28 +1,8 @@
-const cats = [
-  {
-    id: 1,
-    name: "Mittens",
-    image: "https://cdn2.thecatapi.com/images/in-CD5LH5.jpg",
-    description: "A very cute and fluffy cat.",
-  },
-  {
-    id: 2,
-    name: "Whiskers",
-    image: "https://cdn2.thecatapi.com/images/5txKBK89Y.jpg",
-    description: "Loves to play with yarn.",
-  },
-  {
-    id: 3,
-    name: "Shadow",
-    image: "https://cdn2.thecatapi.com/images/Z5Wd0A-r_.jpg",
-    description: "Enjoys napping in the sun.",
-  },
-];
+import cats from "./data/catsArr";
 
 function App() {
-  return (
-    <h1>App</h1>
-  );
+  console.log(cats);
+  return <h1>App</h1>;
 }
 
 export default App;
